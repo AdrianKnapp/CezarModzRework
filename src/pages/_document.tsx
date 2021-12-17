8038e17de113d6f99e396f684743afc52af836aa
Adrian Knapp <adrknapp@gmail.com>
2021-12-17 11:14:28 -0300

    Added flex to buttons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
             <link
               href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;900&display=swap"
               rel="stylesheet"
+              as="font"
             />
 
             <link rel="shortcut icon" href="/favicon.png" type="image/png" />
