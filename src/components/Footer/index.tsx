0330158c9f7081fd24e8b6bc5929d2d12ed1009a
Adrian Knapp <adrknapp@gmail.com>
2021-12-17 11:54:28 -0300

    Added links to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,18 +6,48 @@ export default function Footer() {
       <div className={styles.footerContent}>
         <div className={styles.left}>
           <h2>Contato via E-Mail</h2>
-          <a href="mailto:contato@cezarmodz.com.br">Contato comercial</a>
-          <a href="mailto:suporte@cezarmodz.com.br">Suporte ao cliente</a>
+          <a
+            href="mailto:viniciuslyra98@gmail.com.br"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contato comercial
+          </a>
+          <a
+            href="mailto:devcezarmodz@gmail.com.br"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Suporte ao cliente
+          </a>
         </div>
         <div className={styles.right}>
           <h2>Redes Sociais</h2>
-          <a href="mailto:contato@cezarmodz.com.br">Instagram</a>
-          <a href="mailto:suporte@cezarmodz.com.br">facebook</a>
+          <a
+            href="https://www.instagram.com/cezarmodz/?hl=pt-br"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.facebook.com/CezarMods"
+            target="_blank"
+            rel="noreferrer"
+          >
+            facebook
+          </a>
         </div>
       </div>
       <div className={`container ${styles.footerPolicity}`}>
         <p>Todos os direitos reservados</p>
-        <a href="">Desenvolvedor</a>
+        <a
+          href="https://www.flowcode.com/page/adrianknapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Desenvolvedor
+        </a>
       </div>
     </footer>
   );
