3b81adf5796e6721d4f783f41d9fedc1400c55d9
Adrian Knapp <adrknapp@gmail.com>
2021-12-20 15:06:46 -0300

    Fixed aboutus text

--- src/pages/sobrenos/index.tsx
+++ src/pages/sobrenos/index.tsx
@@ -22,15 +22,12 @@ export default function Sobrenos({ partners }: SobrenosProps) {
       <section id="about-us" className={`container ${styles.aboutUs}`}>
         <h1>Quem somos?</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&lsquo;s standard dummy
-          text ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum.
+          A CezarModz é uma empresa que atua no mercado gamer há mais de 1 ano,
+          oferecendo contas e serviços à todo o mundo. Hoje contamos com nossa
+          conta no Instagram com mais de 3 mil seguidores, um site de alto nível
+          e dois canais no youtube que juntos ultrapassam 2 milhões de views.
+          Além de tudo isso, temos em nosso histórico mais de 500 contas
+          vendidas, 100% de aprovação e satisfação.
         </p>
       </section>
       {partnersExist && (
